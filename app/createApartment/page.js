546e8ce34eb06881abb5ac3b4b9d5fff2ffd97ba
Ilesanmi Oluwatobiloba <tobentra32@gmail.com>
2025-08-12 09:01:23 -0500

    Update page.js

--- app/createApartment/page.js
+++ app/createApartment/page.js
@@ -71,7 +71,7 @@ export default function Add() {
       });
 
       const data = await res.json();
-      console.log("data:",data);
+      console.log("data:",data.urls);
       if (data.urls) {
         setLinks((prev) => [...prev, ...data.urls].slice(0, 5)); // max 5
       }
