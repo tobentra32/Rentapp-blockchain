f1a39ee024e75133f6a030ec29ae9a943588b853
Oluwatobiloba <tobentra32@gmail.com>
2025-08-13 21:05:32 -0500

    swiper updt

--- app/components/ImageSlider.jsx
+++ app/components/ImageSlider.jsx
@@ -1,6 +1,3 @@
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay, Pagination, Navigation } from 'swiper'
