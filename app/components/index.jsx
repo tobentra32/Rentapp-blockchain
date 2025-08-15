df28608a96bb4f833c6bc7ea6596834dd6035e27
Oluwatobiloba <tobentra32@gmail.com>
2025-08-15 15:03:45 -0500

    apartmentId

--- app/components/index.jsx
+++ app/components/index.jsx
@@ -1,13 +1,6 @@
-import Card from './Card'
 import Header from './Header'
 import Category from './Category'
 import ConnectBtn from './ConnectBtn'
-import Collection from './Collection'
-import ImageSlider from './ImageSlider'
-import Review from './Review'
-import Description from './Description'
-import ImageGrid from './ImageGrid'
-import Title from './Title'
 import Footer from './Footer'
 
 
@@ -15,12 +8,5 @@ export {
   Header,
   ConnectBtn,
   Category,
-  Card,
-  Collection,
-  ImageSlider,
-  Review,
-  Description,
-  ImageGrid,
-  Title,
   Footer,
 }
